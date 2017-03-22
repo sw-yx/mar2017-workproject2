6f0a19ce0403f26e20fa9fc0176b9ba1cbf198bc
sw-yx <shawnthe1@gmail.com>
2017-03-22 00:14:10 -0400

    fix heroku deploy port issue

--- index.js
+++ index.js
@@ -35,6 +35,6 @@ app.use('/openapi', openapiRoutes);
 
 
 // start the server
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('Server is now running on http://localhost:3000 or http://127.0.0.1:3000');
 });
